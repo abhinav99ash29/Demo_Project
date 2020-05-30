976a00d23ba9a54dbf5ca49d6beba0868c62422d
Abhinav Gupta <abhinav99ash29@gmail.com>
2020-05-30 11:45:47 +0530

    [properties] Updated bucket credentials in application.properties

--- src/main/java/com/demoProject/demoDB/DemoDbApplication.java
+++ src/main/java/com/demoProject/demoDB/DemoDbApplication.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-
 import com.demoProject.demoDB.model.Customer;
 import com.demoProject.demoDB.repo.CustomerRepository;
 
--- src/main/java/com/demoProject/demoDB/repo/CustomerRepository.java
+++ src/main/java/com/demoProject/demoDB/repo/CustomerRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
 import org.springframework.data.couchbase.core.query.ViewIndexed;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+
 import com.demoProject.demoDB.model.Customer;
 
 @N1qlPrimaryIndexed
 @ViewIndexed(designDoc = "customer", viewName = "all")
 public interface CustomerRepository extends CouchbaseRepository<Customer, String> {
+	
 	List<Customer> findByLastName(String name);
 }
